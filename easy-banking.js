fed6109f4b0d9a1aa02985f7728504ae29d0afb9
swizzi <111010786+SwizzScript@users.noreply.github.com>
2024-07-09 14:05:02 +0100

    JS fixes

--- easy-banking.js
+++ easy-banking.js
@@ -4,17 +4,16 @@ const easyHero = document.getElementById("imageHero");
 const imageMockup = document.getElementById("imageMockup");
 
 
-if (window.innerWidth < 1000) {
-  easyNav.style.display = 'none';
-} else {
-  easyNav.style.display = 'flex';
-}
-
-
+// if (window.innerWidth < 1000) {
+//   easyNav.style.display = 'none';
+// } else {
+//   easyNav.style.display = 'flex';
+// }
+easyNav.style.display = 'none';
 
 window.addEventListener('resize', () => {
  if (window.innerWidth < 1000) {
-   easyHero.src = "./images/bg-intro-mobile.svg"; 
+   easyHero.src = "./images/bg-intro-mobile.svg";
  } else {
   easyNav.style.display = 'none';
   easyHero.src = "./images/bg-intro-desktop.svg";
