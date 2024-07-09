1815d3a28492d2f92e18cbeb9ee7a2d3db7dfb82
swizzi <111010786+SwizzScript@users.noreply.github.com>
2024-07-09 04:05:04 +0100

    first commit

---
+++ easy-banking.js
@@ -0,0 +1,39 @@
+const easyNav = document.getElementById("easyNavbar");
+const easyHamburger = document.getElementById("hambuger");
+const easyHero = document.getElementById("imageHero");
+const imageMockup = document.getElementById("imageMockup");
+
+
+if (window.innerWidth < 1000) {
+  easyNav.style.display = 'none';
+} else {
+  easyNav.style.display = 'flex';
+}
+
+
+
+window.addEventListener('resize', () => {
+ if (window.innerWidth < 1000) {
+   easyHero.src = "./images/bg-intro-mobile.svg"; 
+ } else {
+  easyNav.style.display = 'flex';
+  easyHero.src = "./images/bg-intro-desktop.svg";
+ }
+})
+
+
+easyHamburger.addEventListener('click', () => {
+  if (easyHamburger.getAttribute('switch') === 'closed') {
+    easyNav.style.display = 'flex';
+    easyHamburger.setAttribute('switch', 'opened');
+    easyHamburger.src = "./images/icon-close.svg";
+    document.body.style.overflow = 'hidden';
+    document.documentElement.style.overflowY = 'hidden';
+  } else {
+    easyNav.style.display = 'none';
+    easyHamburger.setAttribute('switch', 'closed');
+    easyHamburger.src = "./images/icon-hamburger.svg";
+    document.body.style.overflow = 'auto';
+    document.documentElement.style.overflowY = 'auto';
+  }
+})
